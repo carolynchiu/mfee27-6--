20ae6257afb1a780b75d0d35e5a54795335559d6
Carolyn <c920231@gmail.com>
2022-07-07 14:06:22 +0800

    update

--- module/header.php
+++ module/header.php
@@ -3,6 +3,6 @@
     <!-- flex-shrink-0 不希望被壓縮可以設定 -->
     <a class="site-name text-white bg-dark p-2 flex-shrink-0" href="">ADMIN</a>
     <input type="text" class="form-control rounded-0" />
-    <a href="doLogout.php" class="btn btn-dark text-nowrap rounded-0">Sign out</a>
+    <a href="doLogout.php" class="btn btn-dark text-nowrap rounded-0">登出</a>
   </div>
 </header>

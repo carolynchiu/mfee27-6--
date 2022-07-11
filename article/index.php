2096da6b9c34f31832e3164ad720329790452f0a
Alex <kon313083@gmail.com>
2022-07-11 16:43:59 +0800

    樣式修改

--- article/index.php
+++ article/index.php
@@ -1,13 +1,63 @@
-<!DOCTYPE html>
+<?php
+session_start();
+require("../db-connect.php");
+
+if (isset($_GET["page"])) {
+  $page = $_GET["page"];
+} else {
+  $page = 1;
+}
+
+// $sqlAll = "SELECT * FROM admins WHERE valid=1";
+// $resultAll = $conn->query($sqlAll);
+// // $adminCount = $resultAll->num_rows; //所有的使用者
+
+// $page = 3;
+// $perPage = 5;
+// $start = ($page - 1) * $perPage;
+
+// $order = $_GET["order"];
+$order = isset($_GET["order"]) ? $_GET["order"] : 1; //給預設值
+
+switch ($order) {
+  case 1:
+    $orderType = "id ASC";
+    break;
+  case 2:
+    $orderType = "id DESC";
+    break;
+  case 3:
+    $orderType = "account ASC";
+    break;
+  case 4:
+    $orderType = "account DESC";
+    break;
+  default:
+    $orderType = "ASC";
+}
+
+
+?>
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Article</title>
-    </head>
-    <body>
-        <h3>新增文章</h3>
+
+<head>
+  <title>Admins</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="../fontawesome-free-6.1.1-web/css/all.min.css">
+  <link rel="stylesheet" href="../style/common.css">
+</head>
+
+<body>
+  <?php require("../module/header.php"); ?>
+  <?php require("../module/aside.php"); ?>
+  <div class="main-content">
+    <h3>新增文章</h3>
         <input id="create" type="text" />
         <button id="create-submit">送出</button>
         <table id="article-data">
@@ -20,6 +70,9 @@
             </tr>
         </table>
         <div id="update-div"></div>
+    </div>
+
+
         <script
             src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
@@ -159,5 +212,7 @@
             };
             //#endregion
         </script>
-    </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
 </html>

fccabb2dfb190a250bf5ead7ba4fa17381a8dcdc
Carolyn <c920231@gmail.com>
2022-07-11 22:56:58 +0800

    update navbar

--- module/header.php
+++ module/header.php
@@ -1,8 +1,50 @@
 <header class="fixed-top">
-  <div class="d-flex">
-    <!-- flex-shrink-0 不希望被壓縮可以設定 -->
+  <!-- <div class="d-flex">
     <a class="site-name text-white bg-dark p-2 flex-shrink-0" href="">ADMIN</a>
     <input type="text" class="form-control rounded-0" />
-    <a href="doLogout.php" class="btn btn-dark text-nowrap rounded-0">登出</a>
-  </div>
+    <a href="/mfee27-group6/doLogout.php" class="btn btn-dark text-nowrap rounded-0">登出</a>
+  </div> -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark p-0">
+    <div class="container-fluid">
+      <a class="navbar-brand site-name" href="#">後台管理系統</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="http://localhost/mfee27-group6/homepage.php">首頁</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost/mfee27-group6/admin/admins.php">管理員</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost/mfee27-group6/user/users.php">會員</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost/mfee27-group6/product/products-list.php">商品</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost/mfee27-group6/order/order-list.php">訂單</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">優惠券</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost/mfee27-group6/recipe/recipe-all.php">食譜</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost/mfee27-group6/course/course.php">課程</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">文章</a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li> -->
+        </ul>
+      </div>
+      <div><a href="/mfee27-group6/doLogout.php" class="btn btn-sm btn-light">登出</a></div>
+    </div>
+  </nav>
 </header>

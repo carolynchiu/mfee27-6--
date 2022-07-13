fdea39c8019b7f4baba11dcf1f0c82c1465f8825
Carolyn <c920231@gmail.com>
2022-07-13 14:19:31 +0800

    update

--- coupon/coupon.php
+++ coupon/coupon.php
@@ -235,7 +235,7 @@ $totalPage = ceil($couponCount / $perPage);
                 <td class="text-end text-danger">$<?= $row["discount"] ?></td>
                 <td class="text-center">
                   <a href="coupon-detail.php?id=<?= $row["id"] ?>" class="btn btn-sm btn-primary"><i class="fa-solid fa-eye me-2"></i>詳細資料</a>
-                  <a href="" class="btn btn-sm btn-warning"><i class="fa-solid fa-pen-to-square me-2"></i>修改</a>
+                  <a href="coupon-edit.php?id=<?= $row["id"] ?>" class="btn btn-sm btn-warning"><i class="fa-solid fa-pen-to-square me-2"></i>修改</a>
                   <a href="doDelete.php?id=<?= $row["id"] ?>" class="btn btn-sm btn-danger"><i class="fa-solid fa-circle-minus me-2"></i>刪除</a>
                 </td>
                 <!-- 連結資料 -->

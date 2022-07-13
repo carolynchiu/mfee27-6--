fc912c409c56867fd82805bec8929e6c201f3962
Wan <tinklin4@gmail.com>
2022-07-13 10:03:28 +0800

    icon

--- recipe/recipe-search.php
+++ recipe/recipe-search.php
@@ -28,19 +28,20 @@ $rows=$result->fetch_all(MYSQLI_ASSOC);
 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../fontawesome-free-6.1.1-web/css/all.min.css" />
 
   </head>
   <body>
       <div class="container">
         <div class="py-2">
-        <a class="btn btn-info" href="recipe-all.php">所有食譜</a>
+        <a class="btn btn-info" href="recipe-all.php"><i class="fa-solid fa-circle-arrow-left me-2"></i>回所有食譜</a>
         </div>
 
         <div class="py-2">
         <form action="recipe-search.php" method="get">
           <div class="input-group">
             <input type="text" name="search" class="form-control">
-            <button type="submit" class="btn btn-info">搜尋</button>
+            <button type="submit" class="btn btn-info"><i class="fa-solid fa-magnifying-glass me-3"></i>搜尋</button>
           </div>
           </form>
         </div>
@@ -60,10 +61,10 @@ $rows=$result->fetch_all(MYSQLI_ASSOC);
                         <h2 class="mb-2"><?=$row["title"]?></h2>
                         <div class="text-start"><?=$row["user_name"]?></div>
                         <div class="py-2 d-grid">
-                            <a class="btn btn-info" href="recipe-detail.php?recipe_id=<?=$row["id"]?>">查看食譜</a>
+                            <a class="btn btn-info" href="recipe-detail.php?recipe_id=<?=$row["id"]?>"><i class="fa-solid fa-carrot me-2"></i>查看食譜</a>
                         </div>
                         <div class="py-2 d-grid">
-                            <a class="btn btn-danger" href="do-delete.php?recipe_id=<?=$row["id"]?>">刪除食譜</a>
+                            <a class="btn btn-danger" href="do-delete.php?recipe_id=<?=$row["id"]?>"><i class="fa-solid fa-xmark me-2"></i> 刪除食譜</a>
                         </div>
 
                     </div>
